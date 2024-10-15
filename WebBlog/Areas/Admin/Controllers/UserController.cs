e13e0ab25c629f39fd1917f6c709f61403b0db1c
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-10-15 12:17:44 +0700

    ResetPassword

--- WebBlog/Areas/Admin/Controllers/UserController.cs
+++ WebBlog/Areas/Admin/Controllers/UserController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 using WebBlog.Models;
+using WebBlog.Utilies;
 using WebBlog.ViewModels;
 
 namespace WebBlog.Areas.Admin.Controllers
@@ -32,11 +34,72 @@ namespace WebBlog.Areas.Admin.Controllers
                 Id = x.Id,
                 FirstName = x.FirstName,
                 LastName = x.LastName,
-                UserName = x.UserName
+                UserName = x.UserName,
+                Email = x.Email
             }).ToList();
-            return View(vm);
+            // assigning role
+            foreach (var user in vm)
+            {
+                var singleUser = await _userManager.FindByIdAsync(user.Id);
+                var role = await _userManager.GetRolesAsync(singleUser);
+                user.Role = role.FirstOrDefault();
+            }
+                return View(vm);
         }
-        [HttpGet("login")]
+		[Authorize(Roles = "Admin")]
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View(new RegisterVM());
+		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterVM vm)
+		{
+			if (!ModelState.IsValid) { return View(vm); }
+			var checkUserByEmail = await _userManager.FindByEmailAsync(vm.Email);
+			if (checkUserByEmail != null)
+			{
+				_notification.Error("Email already exists");
+				return View(vm);
+			}
+			var checkUserByUsername = await _userManager.FindByNameAsync(vm.UserName);
+			if (checkUserByUsername != null)
+			{
+				_notification.Error("Username already exists");
+				return View(vm);
+			}
+
+			var applicationUser = new ApplicationUser()
+			{
+				Email = vm.Email,
+				UserName = vm.UserName,
+				FirstName = vm.FirstName,
+				LastName = vm.LastName
+			};
+
+			var result = await _userManager.CreateAsync(applicationUser, vm.Password);
+			if (result.Succeeded)
+			{
+				if (vm.IsAdmin)
+				{
+					await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAdmin);
+				}
+				else
+				{
+					await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAuthor);
+				}
+				_notification.Success("User registered successfully");
+				return RedirectToAction("Index", "User", new { area = "Admin" });
+			}
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(vm);
+		}
+		[HttpGet("login")]
         public IActionResult Login()
         {
             if (!HttpContext.User.Identity!.IsAuthenticated)
@@ -65,6 +128,51 @@ namespace WebBlog.Areas.Admin.Controllers
             _notification.Success("Login Successful");
             return RedirectToAction("Index", "User", new { area = "Admin" });
         }
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> ResetPassword(string id)
+        {
+            var existingUser = await _userManager.FindByIdAsync(id);
+            if (existingUser == null)
+            {
+                _notification.Error("User doesn't exist");
+                return View();
+            }
+
+            // Tạo đối tượng ViewModel và gán giá trị cho UserName
+            var vm = new ResetPasswordVM()
+            {
+                Id = existingUser.Id,
+                UserName = existingUser.UserName // Lấy UserName của người dùng
+            };
+
+            // Trả vm về View
+            return View(vm);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+            var existingUser = await _userManager.FindByIdAsync(vm.Id);
+            if (existingUser == null)
+            {
+                _notification.Error("User doesnot exist");
+                return View(vm);
+            }
+            var token = await _userManager.GeneratePasswordResetTokenAsync(existingUser);
+            var result = await _userManager.ResetPasswordAsync(existingUser, token, vm.NewPassword);
+            if (result.Succeeded)
+            {
+                _notification.Success("Password reset succuful");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
+        }
+
+
+
         [HttpPost]
         public IActionResult Logout()
         {
