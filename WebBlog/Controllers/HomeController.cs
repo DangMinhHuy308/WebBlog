074c31ac9daa9e2fbbfe510312ea9e2a28c5559e
DangMinhHuy308 <huyminhdang5@gmail.com>
2024-10-17 15:10:26 +0700

    blog detail

--- WebBlog/Controllers/HomeController.cs
+++ WebBlog/Controllers/HomeController.cs
@@ -1,21 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebBlog.Data;
 using WebBlog.Models;
+using WebBlog.ViewModels;
 
 namespace WebBlog.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                                ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM();
+            var setting = _context.Settings!.ToList();
+            vm.Title = setting[0].SiteName;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts = _context.Posts!.Include(x=>x.ApplicationUser).ToList(); 
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ WebBlog/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using WebBlog.Models;
+
+namespace WebBlog.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
